fe5fe2ef5d5912b818cb4117f3abc1999bc25d1d
matsumoto2004 <mamamaturi57@gmail.com>
2025-02-24 00:00:07 +0900

    #15 finished

--- run.sh
+++ run.sh
@@ -1,18 +1,28 @@
 #!/bin/bash
 set -xue
 
+OBJCOPY=/opt/homebrew/opt/llvm/bin/llvm-objcopy
+
 # clangのパス (Ubuntuの場合は CC=clang)
 CC=/opt/homebrew/opt/llvm/bin/clang
 
 CFLAGS="-std=c11 -O2 -g3 -Wall -Wextra --target=riscv32 -ffreestanding -nostdlib"
 
+# シェルをビルド
+$CC $CFLAGS -Wl,-Tuser.ld -Wl,-Map=shell.map -o shell.elf shell.c user.c common.c
+$OBJCOPY --set-section-flags .bss=alloc,contents -O binary shell.elf shell.bin
+$OBJCOPY -Ibinary -Oelf32-littleriscv shell.bin shell.bin.o
+
 # カーネルをビルド
 $CC $CFLAGS -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf \
-    kernel.c common.c
+    kernel.c common.c shell.bin.o
 
 # QEMUのファイルパス
 QEMU=qemu-system-riscv32
 
 # QEMUを起動
 $QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot \
+    -d unimp,guest_errors,int,cpu_reset -D qemu.log \
+    -drive id=drive0,file=lorem.txt,format=raw,if=none \
+    -device virtio-blk-device,drive=drive0,bus=virtio-mmio-bus.0 \
     -kernel kernel.elf
